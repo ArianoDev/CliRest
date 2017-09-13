b2c3c4e674ef5c9b8d284f4cb864e14c1da7dd01
Fabrizio Ariano <fariano@swrhi.it>
2017-09-13 12:21:35 +0200

    Updated AuthorRepository and UserRepository

--- src/main/java/com/fadev/colibri/repo/AuthorRepository.java
+++ src/main/java/com/fadev/colibri/repo/AuthorRepository.java
@@ -1,3 +1,11 @@
+/*
+ * GNU GENERAL PUBLIC LICENSE
+ * Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ */
 package com.fadev.colibri.repo;
 
 import com.fadev.colibri.bean.Author;
@@ -6,7 +14,20 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
+/**
+ * Author Repository Interface used to save {@link Author} objects to MondoDB
+ * and retrieve {@link Author} objects from it.
+ *
+ * @author Fabrizio Ariano.
+ */
 public interface AuthorRepository extends MongoRepository<Author, String> {
 
-    Author findByLastName(@Param("lastname") String lastname);
+    /**
+     * Finds the {@link Author} entry with has input Last Name or input First Name.
+     * @param lastname last name value of {@link Author} to be found
+     * @param firstname first name value of {@link Author} to be found
+     * @return the list of all {@link Author} entries which have last name or first name, equal to input values.
+     */
+    List<Author> findByLastNameOrFirstNameAllIgnoreCase(String lastname, String firstname);
+
 }
--- src/main/java/com/fadev/colibri/repo/UserRepository.java
+++ src/main/java/com/fadev/colibri/repo/UserRepository.java
@@ -1,7 +1,21 @@
+/*
+ * GNU GENERAL PUBLIC LICENSE
+ * Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ */
 package com.fadev.colibri.repo;
 
 import com.fadev.colibri.bean.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+/**
+ * User Repository Interface used to save {@link User} objects to MondoDB
+ * and retrieve {@link User} objects from it.
+ *
+ * @author Fabrizio Ariano.
+ */
 public interface UserRepository extends MongoRepository<User, String> {
 }
